34ad35fe5e1fafa2704424ec8c5d998022fbeb8f
zzy <zzy@zzydeMacBook-Pro.local>
2015-07-18 21:16:09 +0800

    Initial Commit

---
+++ MinutesToMidnight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MinutesToMidnight
+//
+//  Created by zzy on 15/7/18.
+//  Copyright (c) 2015年 zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MinutesToMidnight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MinutesToMidnight
+//
+//  Created by zzy on 15/7/18.
+//  Copyright (c) 2015年 zzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
